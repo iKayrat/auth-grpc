341378a0336c1db4094c7ccddfbeb63f3086b9bc
k isaev <ik.kairat@gmail.com>
2023-11-27 23:44:54 +0600

    add constants

---
+++ internal/inmemory/constants.go
@@ -0,0 +1,7 @@
+package storage
+
+const (
+	AdminUUID          = "e257152c-43e2-4fe2-8cd3-0618006890c5"
+	UserNotFound       = "user not found"
+	UserWithIdNotFound = "user with ID \"%s\" not found"
+)
